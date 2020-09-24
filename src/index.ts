12442eda87d1aeea64d052016815a2f34f010a46
Moritz Zier <moritz.zier@web.de>
2020-09-24 12:31:16 +0200

    fixed environment

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const app = express();
 
+console.log(process.env.PORT)
+
 app.listen(process.env.PORT, () => {
-    `Server is listening to ${process.env.PORT}`
+    console.log(`Server is listening to ${process.env.PORT}`)
 })
 
 app.get('*', (req, res) => {
